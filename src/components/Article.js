4bf77273d5386097680cb3179baf1d15fa60abec
KRisHnA <kkp785216@gmail.com>
2022-09-10 14:53:52 +0530

    article compnent improved

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import LazyLoad from 'react-lazy-load';
 
 const formatDate = (input) => {
   const date = new Date(input);
@@ -10,7 +11,7 @@ const Article = ({title, img_url, img_comp, date, url, author}) => {
   return (
     <div className="group w-1/2 px-5 py-4" title={title}>
       <Link to={`/${url}`} className="relative w-full block overflow-hidden pb-[50%]">
-        <img className="absolute top-0 left-0 right-0 bottom-0" src={img_url ? img_url : `./img/articles/485x360/${img_comp}.jpg`} alt={title} />
+        <LazyLoad><img className="absolute top-0 left-0 right-0 bottom-0" src={img_url ? img_url : `./img/articles/485x360/${img_comp}.jpg`} alt={title} /></LazyLoad>
         <span className="absolute bottom-0 left-0 text-mywhite bg-black group-hover:bg-blue-500 block w-fit px-1.5 py-0.5 text-10px">New Look</span>
       </Link>
       <h3 className="text-21px leading-7 mt-3 group-hover:text-sky-400"><Link to={`/${url}`}>{title}</Link></h3>
