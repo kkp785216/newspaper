e6cbe8693f1dc04990216d5ebb200e254c845119
KRisHnA <kkp785216@gmail.com>
2022-09-22 17:18:18 +0530

    overall app responsiveness improved

--- src/components/LatestArticles.js
+++ src/components/LatestArticles.js
@@ -26,10 +26,10 @@ const LatestArticles = () => {
   }, [page]);
 
   function scrollToTargetAdjusted() {
-    var element = document.getElementById('latest-articles');
-    var headerOffset = 24;
-    var elementPosition = element.getBoundingClientRect().top;
-    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    let element = document.getElementById('latest-articles');
+    let headerOffset = 24;
+    let elementPosition = element.getBoundingClientRect().top;
+    let offsetPosition = elementPosition + window.pageYOffset - headerOffset;
     window.scrollTo({
       top: offsetPosition,
       behavior: "smooth"
