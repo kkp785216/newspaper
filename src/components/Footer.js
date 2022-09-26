8ce9e6978f4e5476803dc1b36323c08e6489bb2e
KRisHnA <kkp785216@gmail.com>
2022-09-26 16:39:45 +0530

    footer responsiveness improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,13 +26,13 @@ const Footer = () => {
         <footer className='mt-7 md:mt-11'>
             <div className='min-h-[717px] w-full text-white' style={{ background: `linear-gradient(rgb(0 0 0 / 80%), rgb(0 0 0 / 80%)), url("${config.footer_background}") no-repeat top / cover` }}>
                 <div className='md:max-w-screen-md lg:max-w-screen-lg lg1140:max-w-[1068px] px-5 lg:px-[22px] lg1140:px-0 m-auto pt-8 md:pt-11'>
-                    <div className='flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-[28px] lg:space-x-11'>
+                    <div className='flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-[28px] lg:space-x-11'>
                         <div className='w-full md:w-2/3'>
-                            <div className='flex flex-wrap flex-col md:flex-row -mx-5 md:-mx-[10px] lg:-mx-5 -my-4'>
-                                <div className="w-full md:w-1/2 px-5 md:px-[10px] lg:px-5 py-4">
+                            <div className='flex flex-wrap flex-col md:flex-row -mx-5 md:-mx-[10px] lg:-mx-5 md:-my-4'>
+                                <div className="w-full md:w-1/2 px-5 md:px-[10px] lg:px-5 md:py-4">
                                     {footer_editor_choice.length >= 1 && <FooterPosts heading='Editor Choice' articles={footer_editor_choice} />}
                                 </div>
-                                <div className="w-full md:w-1/2 px-5 md:px-[10px] lg:px-5 py-4">
+                                <div className="w-full md:w-1/2 px-5 md:px-[10px] lg:px-5 py-1 md:py-4">
                                     {footer_most_popular.length >= 1 && <FooterPosts heading='Popular Posts' articles={footer_most_popular} />}
                                 </div>
                             </div>
@@ -56,17 +56,17 @@ const Footer = () => {
                         <div className='w-full md:w-2/3'>
                             <div className='flex flex-col md:flex-row -mx-5 md:-mx-[10px] lg:-mx-5 -my-4'>
                                 <div className='w-full md:w-[38%] px-5 md:px-[10px] lg:px-5 py-4'>
-                                    <Link to='/'><LazyLoad><img src="/img/newspaper-11-logo-white.png" alt="" /></LazyLoad></Link>
+                                    <Link to='/'><LazyLoad><img className='block mx-auto md:mx-0' src="/img/newspaper-11-logo-white.png" alt="" /></LazyLoad></Link>
                                 </div>
                                 <div className='w-full md:w-[62%] px-5 md:px-[10px] lg:px-5 py-4'>
-                                    <h4 className='text-[19px] mb-7 font-semibold uppercase'>About Us</h4>
-                                    <p className='text-[14.5px] mb-5'>Newspaper is your news, entertainment, music fashion website. We provide you with the latest breaking news and videos straight from the entertainment industry.</p>
+                                    <h4 className='text-[19px] mb-7 font-semibold uppercase text-center md:text-left  pt-3 md:pt-0'>About Us</h4>
+                                    <p className='text-[14px] md:text-[13.5px] lg:text-[14.5px] mb-5'>Newspaper is your news, entertainment, music fashion website. We provide you with the latest breaking news and videos straight from the entertainment industry.</p>
                                     <span className='text-[14.5px]'>Contact us: <a className='text-sky-400' href='mailto:contact@yoursite.com'>contact@yoursite.com</a></span>
                                 </div>
                             </div>
                         </div>
                         <div className='w-full md:w-1/3'>
-                            <h4 className='text-[19px] mb-7 font-semibold uppercase'>Follow Us</h4>
+                            <h4 className='text-[19px] mb-7 font-semibold uppercase text-center md:text-left pt-3 md:pt-0'>Follow Us</h4>
                             <div>
 
                             </div>
--- src/components/FooterPosts.js
+++ src/components/FooterPosts.js
@@ -8,8 +8,8 @@ const FooterPosts = ({ heading, articles }) => {
       <h4 className='text-[19px] mb-5 font-medium uppercase'>{heading}</h4>
       {articles.map((e, i) => (
         <div className="flex space-x-4 md:space-x-3 lg:space-x-4 group mb-7 md:mb-6" title={e.title} key={i}>
-          <Link className="w-[30%]" to={`/${e.url}`}><LazyLoad><img className="w-full object-cover" src={e.img_url ? e.img_url : `/img/articles/218x150/${e.img_comp}.jpg`} alt={e.title} /></LazyLoad></Link>
-          <div className="w-[70%]">
+          <Link className="w-1/3 md:w-[30%]" to={`/${e.url}`}><LazyLoad><img className="w-full object-cover" src={e.img_url ? e.img_url : `/img/articles/218x150/${e.img_comp}.jpg`} alt={e.title} /></LazyLoad></Link>
+          <div className="w-2/3 md:w-[70%]">
             <h3 className="text-sm md:text-[12.5px] lg:text-sm md:leading-5 text-white group-hover:text-sky-400 font-medium"><Link to={`/${e.url}`}>{e.title}</Link></h3>
             <span className="text-11px font-medium text-gray-500">August 7, 2019</span>
           </div>
