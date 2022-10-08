3346dc961a35fc8fe29d7dc6d6642f86205ab8f3
KRisHnA <kkp785216@gmail.com>
2022-10-08 19:52:02 +0530

    router folder changed for vercel

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.use(express.static(path.join(`${__dirname}/public`)));
 
 app.use(imageRoutes);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 });
