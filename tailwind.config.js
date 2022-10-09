d8a2fb70167ec3d0961d3ab8f4bae3e36299ac43
KRisHnA <kkp785216@gmail.com>
2022-10-09 14:11:51 +0530

    footer and header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/*.{html,js,ejs}",
+    "./views/**/*.{html,js,ejs}",
     "./public/*.{html,js}",
 ],
   theme: {
