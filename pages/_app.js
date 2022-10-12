70cbb3987d3ed60ca96d428f8b2cd9c6ac7366ac
KRisHnA <kkp785216@gmail.com>
2022-10-12 15:17:59 +0530

    redux implemented with next-redux-wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import { useEffect, useRef } from "react";
 import '../styles/globals.css'
 import { useRouter } from 'next/router'
-import { Provider } from 'react-redux'
-import store from '../redux/store';
 import Header from "../components/Header";
 import { useDispatch } from "react-redux";
 import { preload } from "../redux/preload";
 import Footer from "../components/Footer";
+import store from "../redux/store";
 
 const Layout = ({ children, app }) => {
 
@@ -17,8 +16,6 @@ const Layout = ({ children, app }) => {
   }, []);
 
   const router = useRouter();
-  const { post } = router.query
-  console.log(router)
 
   switch (router.route) {
     case '/':
@@ -44,13 +41,13 @@ function MyApp({ Component, pageProps }) {
   const app = useRef();
   return (
     <div ref={app} className="App [&.active]:scale-[.9] origin-[50%_200px_0] transition-all duration-700 [&.active]:shadow-[0_0_46px_#000]">
-      <Provider store={store}>
-        <Layout app={app}>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
+      <Layout app={app}>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   )
 }
 
-export default MyApp
+// export default MyApp
+
+export default store.withRedux(MyApp);
--- redux/store.js
+++ redux/store.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import { reducer } from './reducer'
 import { initialState } from "./reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { createWrapper } from "next-redux-wrapper";
 
 const store = createStore(
     reducer,
@@ -10,4 +11,9 @@ const store = createStore(
     composeWithDevTools(applyMiddleware(thunk))
 );
 
-export default store;
+// export default store;
+
+export const makeStore = () => store
+
+export const wrapper = createWrapper(makeStore);
+export default wrapper;
