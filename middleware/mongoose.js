f3760c0d85d1c225a29b12bb76bea23aa4858701
KRisHnA <kkp785216@gmail.com>
2022-10-16 13:33:12 +0530

    environment variable

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectDB = handler => async (req, res) => {
     if(mongoose.connections[0].readyState) {
         return handler(req, res);
     }
-    await mongoose.connect(`${$process.env.MONGO_URI}`);
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     // await mongoose.connect('mongodb://localhost:27017/newspaper11');
     // await mongoose.connect('mongodb+srv://kkp785215database1:9721179735@cluster0.l2ybizl.mongodb.net/newspaper11');
     return handler(req, res);
