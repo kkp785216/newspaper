1dcdb23150f536ceb0537331319d7e40fd7460c4
KRisHnA <kkp785216@gmail.com>
2022-10-16 13:22:19 +0530

    attached mongodb as a dynamic data

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,10 +4,10 @@ const connectDB = handler => async (req, res) => {
     if(mongoose.connections[0].readyState) {
         return handler(req, res);
     }
+    await mongoose.connect(process.env.MONGO_URI);
     // await mongoose.connect('mongodb://localhost:27017/newspaper11');
-    await mongoose.connect('mongodb+srv://kkp785215database1:9721179735@cluster0.l2ybizl.mongodb.net/newspaper11');
+    // await mongoose.connect('mongodb+srv://kkp785215database1:9721179735@cluster0.l2ybizl.mongodb.net/newspaper11');
     return handler(req, res);
 }
-console.log(process.env.MONGO_URI)
 
 export default connectDB;
