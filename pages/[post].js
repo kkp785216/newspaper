33f223bd37323890ca9cfb4ea0615329d2727396
KRisHnA <kkp785216@gmail.com>
2022-11-10 14:05:16 +0530

    post rendered on server side using getstaticprops and getstaticpaths

--- pages/[post].js
+++ pages/[post].js
@@ -1,26 +1,28 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+// import { useEffect, useState } from 'react'
+// import { useRouter } from 'next/router'
 import Template1 from '../components/Template1'
 import fetchapi from '../lib/api'
 
-const Post = () => {
-  const router = useRouter()
-  const { post } = router.query
-  const [article, setArticle] = useState(null);
-  const [nextprev, setNextPrev] = useState(null);
+const Post = ({ article, nextprev }) => {
+  // const router = useRouter()
+  // const { post } = router.query
+  // const [article, setArticle] = useState(null);
+  // const [nextprev, setNextPrev] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      if (post !== undefined) {
-        try {
-          let article = await fetchapi(`getarticles?uses=singlearticle&slug=${post}`);
-          let prevnext = await fetchapi(`getarticles?uses=prevnext&slug=${post}`);
-          setArticle(article.article);
-          setNextPrev(prevnext);
-        } catch (error) { console.log(error.message) }
-      }
-    })();
-  }, [post]);
+  // useEffect(() => {
+  //   (async () => {
+  //     if (post !== undefined) {
+  //       try {
+  //         let article = await fetchapi(`getarticles?uses=singlearticle&slug=${post}`);
+  //         let prevnext = await fetchapi(`getarticles?uses=prevnext&slug=${post}`);
+  //         setArticle(article.article);
+  //         setNextPrev(prevnext);
+  //         console.log(article.article, "this is article")
+  //         console.log(data, "this is data")
+  //       } catch (error) { console.log(error.message) }
+  //     }
+  //   })();
+  // }, [post]);
 
   return (
     <div>
@@ -29,12 +31,23 @@ const Post = () => {
   )
 }
 
-// export async function getServerSideProps(context) {
-//   const { req, query, res, asPath, pathname } = context;
-//   let article = await fetchapi(`getarticles?uses=singlearticle&slug=${query.post}`, req.headers.host);
-//   return {
-//     props: {data: article}, // will be passed to the page component as props
-//   }
-// }
+export async function getStaticPaths(context) {
+  const routes = await fetchapi('getroutes?uses=articles', 'localhost:3000');
+  return {
+    paths: routes.routes.map((route) => {
+      return { params: { post: route.url } }
+    }),
+    fallback: false, // can also be true or 'blocking'
+  }
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  let article = await fetchapi(`getarticles?uses=singlearticle&slug=${params.post}`, 'localhost:3000');
+  let nextprev = await fetchapi(`getarticles?uses=prevnext&slug=${params.post}`, 'localhost:3000');
+  return {
+    props: { article: article.article, nextprev }, // will be passed to the page component as props
+  }
+}
 
 export default Post
