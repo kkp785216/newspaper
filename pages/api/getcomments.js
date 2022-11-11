210ccdfbecbdd18bcc323fa0c755c6b697869095
KRisHnA <kkp785216@gmail.com>
2022-11-11 14:56:56 +0530

    get comment api builded

---
+++ pages/api/getcomments.js
@@ -0,0 +1,23 @@
+import comment from '../../models/comment'
+import connectDB from "../../middleware/mongoose"
+
+const handler = async (req, res) => {
+    const maxLimit = 20;
+    if (req.query.post && req.query.limit && req.query.page) {
+        const limit = req.query.limit <= maxLimit ? req.query.limit : maxLimit
+        let count = await comment.find({post: req.query.post}).count();
+        let comments = await comment.find({post: req.query.post}).limit(limit).skip((req.query.page - 1) * limit);
+        res.status(200).json({
+            comments,
+            page: parseInt(req.query.page),
+            limit: parseInt(req.query.limit),
+            max_limit: maxLimit,
+            total_comments: count,
+        });
+    }
+    else {
+        res.status(404).json({ error: 'Please provide a valid parameter' });
+    }
+}
+
+export default connectDB(handler);
