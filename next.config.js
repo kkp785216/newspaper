17f2cf8840cbdeae65a27cd3d7dd6eb30aef27e3
KRisHnA <kkp785216@gmail.com>
2022-12-04 17:56:36 +0530

    app on vercel still giving cross error tried to fix

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/api/.*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
