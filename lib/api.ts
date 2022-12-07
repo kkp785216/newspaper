74d56fe152414b7fe2a218af5e89706ca95a4cf8
KRisHnA <kkp785216@gmail.com>
2022-12-07 22:22:21 +0530

    fetch url over https except localhost

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 const fetchapi = async (query: String, hostname?: String) => {
-    const url = hostname ? `http://${hostname}/api/${query}` : `/api/${query}`
+    const url = hostname ? `${hostname.includes('localhost') ? 'http': 'https'}://${hostname}/api/${query}` : `/api/${query}`
     const res = await fetch(url);
     const data = await res.json();
     return data;
